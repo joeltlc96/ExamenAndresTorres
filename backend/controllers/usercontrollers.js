1d0493012e0a0316e1a3695b026627c706234b0c
joeltlc96 <145817552+joeltlc96@users.noreply.github.com>
2025-12-28 10:17:42 -0500

    fd

--- backend/controllers/usercontrollers.js
+++ backend/controllers/usercontrollers.js
@@ -1,7 +1,9 @@
-export const userView = async (req,res) => {
+export const userView = async (req,res) =>{
     try {
-        const respuesta = awaitfect
+        const respuesta = await fetch(process.env.DB)
+        const data = await respuesta.json();
+        res.status(200).json(data);
     } catch (error) {
-        
+        res.status(500).json({message:'eee',erros:error.message});
     }
 }
